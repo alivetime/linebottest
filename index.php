ef694bf792e97c7a91e61fb7a5d92eb31ee1a18a
陳柏甫 <alivetime2046@gmail.com>
2017-04-19 20:51:45 +0800

    加入linebot程式碼測試

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
 <?php 
-echo "OK";
+$access_token ='NGP+M8biAeW1r03W75esU3p0TcwME+wZcMEpOaeYbIxpgUM712oOJVxt138/akpbJ6EWF5mX8qjWFSGRnxHHop+3R+4tvyVaJzW+2z2FGDc4O//veizPKSzyfqEy6XR4hV+ea+pvGtRSTu6YEzxKuAdB04t89/1O/w1cDnyilFU=';
+//define('TOKEN', '你的Channel Access Token');
+
+$json_string = file_get_contents('php://input');
+
+$file = fopen("C:\\Line_log.txt", "a+");
+fwrite($file, $json_string."\n"); 
+$json_obj = json_decode($json_string);
+
+$event = $json_obj->{"events"}[0];
+$type  = $event->{"message"}->{"type"};
+$message = $event->{"message"};
+$reply_token = $event->{"replyToken"};
+        
+$post_data = [
+  "replyToken" => $reply_token,
+  "messages" => [
+    [
+      "type" => "text",
+      "text" => $message->{"text"}
+    ]
+  ]
+]; 
+fwrite($file, json_encode($post_data)."\n");
+
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$access_token
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+fwrite($file, $result."\n");  
+fclose($file);
+curl_close($ch);
 ?>
